092a9b757341f159c4849cf1c65b6340f8c2e04b
2017029570 <48683310+2017029570@users.noreply.github.com>
2019-06-07 15:36:07 +0900

    0607/Album Update

--- Latte1/DataCenter.swift
+++ Latte1/DataCenter.swift
@@ -9,11 +9,13 @@
 import Foundation
 
 struct Album {
-    let name : String
+    let year : Int
+    let month : Int
+    let day : Int
     var photo : [Any]
 }
 
-var favorite = Album(name:"favorite", photo: ["image1.jpeg", "image2.jpeg"])
-var albums : [Album] = [favorite]
+var A20190531 = Album(year: 2019, month: 5, day: 31, photo: ["image1.jpeg", "image2.jpeg"])
+var albums : [Album] = [A20190531]
 
 
